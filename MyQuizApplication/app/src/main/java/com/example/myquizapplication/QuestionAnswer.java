06ff6997c8a91760774716fbccbf1fe8d78235c9
Mohit Hitendra Bisht <82029867+MohitHitendraBisht@users.noreply.github.com>
2023-03-05 22:22:59 +0530

    Initial commit

---
+++ MyQuizApplication/app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,35 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] = {
+
+            "What does the Internet prefix WWW stand for?",
+
+            "Which company owns the android?",
+
+            "Which of these products is not made by the Apple Corporation?",
+
+            "Who founded Apple Computer?"
+
+    };
+
+    public static String choices[][] = {
+
+            {"Wide Width Wickets", "World Wide Web", "Worldwide Weather", "Western Washington World"},
+            {"Google", "Apple", "Nokia", "Samsung"},
+            {"IMAX" ,  "iPhone" ,  "iMac" ,  "iPod"},
+            {"Stephen Fry", "Bill Gates", "Steve Jobs", "Stephen Hawking"}
+
+    };
+
+    public static String correctAnswers[] = {
+
+            "World Wide Web",
+            "Google",
+            "IMAX",
+            "Steve Jobs"
+
+    };
+
+}
